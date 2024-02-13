e4443f2ee091b75e4fe02d0eeaa55ae1e047785c
Brunella Quaye <131973976+brunellaquaye@users.noreply.github.com>
2024-02-13 18:38:08 +0000

    feat:Add file to explore properties of strings

---
+++ propertiesOfStrings.js
@@ -0,0 +1,13 @@
+//properties of strings
+const name = "John"
+let text = "Please locate where 'locate' occurs!";
+
+console.log("Length of the string : ", name.lenth());
+
+console.log("Index of the string is :", name.charAt(3));
+
+console.log("Index of the String is : ", name.indexOf("n"));
+console.log("Convert string to lowercase : ", name.toLowerCase());
+
+console.log("Locate text in the string :", text,search("locate"));
+
